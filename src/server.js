b3bdc3ce73169f658a8996a1d18e96f53310e88c
Samuel Maia <153786965+samuelmaia1@users.noreply.github.com>
2024-07-24 23:50:38 -0300

    Configurando a saída padrao da api

--- src/server.js
+++ src/server.js
@@ -21,12 +21,14 @@ app.use(express.json())
 app.use(cors())
 
 app.get('/', (req, res) => {
-    res.end(JSON.stringify({
+    return res
+    .setHeader('Content-type', 'application-json')
+    .end(JSON.stringify({
         add_product_url: 'http://localhost:3000/products/new',
         product: 'http://localhost:3000/products/{id}',
         remove_product_url: 'http://localhost:3000/products/remove/{id}',
         update_product_url: 'http://localhost:3000/products/update/{id}'
-    }))
+    }, null, 3))
 })
 
 app.listen(port, () => {
